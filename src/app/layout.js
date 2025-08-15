80df5bfaeadd656c9dcd7f3a74025eb406cf68d6
Sandra <radeskasandra1@gmail.com>
2025-08-15 11:16:47 +0200

    Adapt GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,8 +49,8 @@ export default function RootLayout({ children }) {
         {children}
         <Toaster />
         <Footer />
+        <GoogleAnalytics gaId="G-83G19DTXF2" />
       </body>
-      <GoogleAnalytics gaId="G-83G19DTXF2" />
     </html>
   );
 }
