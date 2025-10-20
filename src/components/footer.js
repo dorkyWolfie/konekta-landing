85214ac59c11ed1522da40c76887cd895a42f105
Sandra <radeskasandra1@gmail.com>
2025-10-20 11:44:55 +0200

    Add contact info to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,8 @@
 import Link from "next/link"
 import Image from "next/image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default async function Footer () {
     return (
@@ -19,6 +22,17 @@ export default async function Footer () {
                         <Link className="hover:text-[#bae6fd]" href={"/uslovi-na-koristenje"}>Услови на користење</Link>
                         <Link className="hover:text-[#bae6fd]" href={"/upatstvo-za-koristenje"}>Упатство за користење</Link>
                         <Link className="hover:text-[#bae6fd]" href={"/reklamacija"}>Политика на поврат и рефундација</Link>
+                    </nav>
+                    <nav className="flex flex-col gap-4">
+                        <p className="font-bold text-lg -mb-2">Контакт</p>
+                        <div className="flex items-center gap-4">
+                            <Link className="hover:text-[#bae6fd]" target="_blank" href={"https://www.facebook.com/profile.php?id=61578597457088"}><FontAwesomeIcon icon={faFacebook} size="lg" /></Link>
+                            <Link className="hover:text-[#bae6fd]" target="_blank" href={"https://www.instagram.com/konektamk"}><FontAwesomeIcon icon={faInstagram} size="lg" /></Link>
+                        </div>
+                        <Link className="hover:text-[#bae6fd] flex items-center gap-2" href={"mailto:info@konekta.mk"}>
+                            <FontAwesomeIcon icon={faEnvelope} size="lg" />
+                            <span>info@konekta.mk</span>
+                        </Link>
                     </nav>
                 </div>
             </div>
